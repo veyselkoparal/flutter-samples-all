ea9e125747b8893a699d929378e4787b06e6e9f3
veyselkoparal <veyselkoparal@gmail.com>
2021-07-12 15:20:33 +0300

    commit

--- lib/samples/httpSample/php_dosyalar/db.php
+++ lib/samples/httpSample/php_dosyalar/db.php
@@ -1,5 +1,5 @@
 <?php
-$db = new mysqli("localhost","admin","1a51450296f3b0cd4dcdf11f6b3d96dfc887a51edb712772","flutter_http");
+$db = new mysqli("localhost","admin","","flutter_http");
 $db->set_charset("utf8");
 if($db->connect_errno){ 
 	die("Üzgünüm, bazı problemler oluştu");
